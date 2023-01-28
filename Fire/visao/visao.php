a8b2f2e14cdcb9e9f53d9c34c8098d4fa98d1366
João Vitor <joaovitorsborges@outlook.com>
2023-01-28 10:02:58 -0300

    atualizar visao

--- Fire/visao/visao.php
+++ Fire/visao/visao.php
@@ -8,7 +8,7 @@ session_start();
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style2.css">
+    <link rel="stylesheet" href="style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
         integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
         crossorigin="anonymous"/>
@@ -90,9 +90,10 @@ session_start();
                                
                                 <textarea class="DE" type="text" placeholder="Detalhes da emergencia" name="mensagem"></textarea><br>
                                 <input class="modal__link" type="submit" name="enviar" >
+                                <a href="visao.php" class="modal__link">Voltar</a> 
                             </form>
                         </p>
-                    <!--<a href="#" class="modal__link">OK</a> -->
+                    
                     
                 </div>
                 
